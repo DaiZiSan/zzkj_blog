b7798642f50dae9c4a124296efc0f454530dfc62
DaiZiSan <daimaizi@192.168.2.59>
2024-11-10 22:19:13 +0800

    Site updated: 2024-11-10 22:19:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/07/hello-world/","2024/11/10/秋思/"];function toRandomPost(){
+var posts=["2024/11/10/秋思/","2024/11/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
